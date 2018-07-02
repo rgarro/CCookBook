2860de0bd9f5c5e2bb2fadd773f007f6792e0e0c
rgarro <rgarro@gmail.com>
2018-07-02 12:07:18 -0600

    will be continued ...

---
+++ Ccook16/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <queque>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    queue<string> q;
+
+    cout << "a queue: ";
+    q.push("one");
+    q.push("two");
+    q.push("three");
+    q.push("four");
+
+    cout << "mas tarde lo seguimos wey"..
+
+    return 0;
+}
